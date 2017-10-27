9e20cb6480dbf6bddd677423727215b32cd1eb58
Ben <benkbaron@gmail.com>
2017-10-26 20:36:30 -0400

    Update border warning style

--- drawcanvas.js
+++ drawcanvas.js
@@ -35,13 +35,19 @@ const DrawCanvas = {
     ctx.fillText(`Lives Left: ${lifeCount}`, 10, 88);
     if (helicopter1.posX < -5 || helicopter1.posX > 900 || helicopter1.posY < -20 || helicopter1.posY > 510) {
       if (helicopter1.alive) {
-      ctx.font = '50px tahoma';
+      ctx.font = 'bold 50px tahoma';
+      ctx.strokeStyle = 'black';
+      ctx.lineWidth = 15;
+      ctx.stroke();
+      ctx.strokeStyle = 'black';
+      ctx.lineWidth = 5;
+      ctx.strokeText("Danger!", 420, 250);
+      ctx.strokeText("Stay in borders!", 320, 350);
       ctx.fillStyle = "red";
       ctx.fillText("Danger!", 420, 250);
-      ctx.fillText("Stay in borders!", 335, 350);
+      ctx.fillText("Stay in borders!", 320, 350);
       }
     }
-
   },
 
   pausedPage(ctx){
